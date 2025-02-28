7b49e413f7fe5c8de8eed5afe6d6cf4ca0009221
Pablo1Gustavo <pablo1gustavo.dev@gmail.com>
2025-02-27 23:11:06 -0300

    feat: refactor and integrate formatter in SeqHandler

--- src/Handler/SeqHandler.php
+++ src/Handler/SeqHandler.php
@@ -1,12 +1,14 @@
 <?php
-declare(strict_types = 1);
-
-namespace Pablo\MonologSeq\Handler;
+declare(strict_types=1);
+namespace Pablo1Gustavo\MonologSeq\Handler;
 
 use Monolog\Handler\AbstractProcessingHandler;
+use Monolog\Handler\Curl\Util as CurlUtil;
 use Monolog\Handler\MissingExtensionException;
 use Monolog\Level;
 use Monolog\LogRecord;
+use Monolog\Formatter\FormatterInterface;
+use Pablo1Gustavo\MonologSeq\Formatter\SeqJsonFormatter;
 
 class SeqHandler extends AbstractProcessingHandler
 {
@@ -14,16 +16,15 @@ class SeqHandler extends AbstractProcessingHandler
     protected readonly string $apiKey;
 
     const SEQ_KEY_HEADER = 'X-Seq-Api-Key';
+    const CLEF_CONTENT_TYPE = 'application/vnd.serilog.clef';
 
     public function __construct(
         string $url,
         string $apiKey,
         Level $level = Level::Info,
-        bool $bubble = true 
-    )
-    {
-        if (!\extension_loaded('curl'))
-        {
+        bool $bubble = true
+    ) {
+        if (!\extension_loaded('curl')) {
             throw new MissingExtensionException('The curl extension is required to use the SeqHandler');
         }
         $this->url = $url;
@@ -31,8 +32,28 @@ class SeqHandler extends AbstractProcessingHandler
         parent::__construct($level, $bubble);
     }
 
-    public function write(LogRecord $record): void
+    protected function write(LogRecord $record): void
     {
+        $this->send($record->formatted);
+    }
 
+    protected function send(string $data): void
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $this->url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-Type: ' . self::CLEF_CONTENT_TYPE,
+            'X-Seq-ApiKey: ' . $this->apiKey,
+        ]);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+        CurlUtil::execute($ch);
+    }
+
+    protected function getDefaultFormatter(): FormatterInterface
+    {
+        return new SeqJsonFormatter();
     }
 }
